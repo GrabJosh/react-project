f898302e969cb144c845920eba9049e924c623c3
GrabJosh <86810589+GrabJosh@users.noreply.github.com>
2024-08-11 14:38:28 -0400

    Update dependencies and fix employee component

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,10 +1,9 @@
 function Employee(props) {
   return (
-    <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+    <div class="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
       <img
-        className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+        class="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
         src={props.img}
-        alt="Woman's Face"
       />
       <div class="text-center space-y-2 sm:text-left">
         <div class="space-y-0.5">
